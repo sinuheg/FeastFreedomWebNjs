c046160ffc6009dfcbd113f4958c4c280456abf0
Sinuhe Guerra Hernandez <sinuhe.guerra@summitworks.com>
2017-07-19 02:53:27 -0400

    first commit

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,11 @@
 module.exports = {
-    remoteUrl: 'mongodb://feastfreedomapp:feastfreedomapp@ec2-34-203-188-157.compute-1.amazonaws.com/feastfreedom',
+    remoteUrl: 'mongodb://feastfreedomapp:feastfreedomapp123@ec2-34-203-188-157.compute-1.amazonaws.com/feastfreedom',
     localUrl: 'mongodb://localhost/feastfreedom'
 }
+
+
+
+//to connect from putty
+//sudo mongo admin -u sinuhe -p sinuhe
+
+//db.createUser({user: "feastfreedomapp",pwd: "feastfreedomapp123",roles:[{"role" : "readWrite","db": "feastfreedom"}]})
