6ced9960c68105716daf9097683cd027b81735d8
Manohar Roy <roymanoharmkster@gmail.com>
2017-07-31 19:21:45 +0530

    login Css changes

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,12 @@
+// module.exports = {
+//     //remoteUrl: 'mongodb://feastfreedomuser:feastfreedomuser123@ec2-34-203-188-157.compute-1.amazonaws.com/feastfreedom',
+//     remoteUrl: 'mongodb://FFUserNjs:FFUserNjs123@ec2-34-204-50-45.compute-1.amazonaws.com/feastfreedom',
+//     localUrl: 'mongodb://localhost/feastfreedom'
+// }
 module.exports = {
-    //remoteUrl: 'mongodb://feastfreedomuser:feastfreedomuser123@ec2-34-203-188-157.compute-1.amazonaws.com/feastfreedom',
-    remoteUrl: 'mongodb://FFUserNjs:FFUserNjs123@ec2-34-204-50-45.compute-1.amazonaws.com/feastfreedom',
+    remoteUrl: 'mongodb://feastfreedomuser:feastfreedomuser123@ec2-34-203-188-157.compute-1.amazonaws.com/feastfreedom',
     localUrl: 'mongodb://localhost/feastfreedom'
 }
-
 
 
 //to connect from putty
