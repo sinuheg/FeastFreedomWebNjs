41612a4e727ad9128242d6ef8cf239078d9122e2
Manohar Roy <roymanoharmkster@gmail.com>
2017-08-01 14:44:56 +0530

    changes

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,42 +1,6 @@
 $(function(){
 
-  "use strict";
-
-  // reload if layout needs to be refreshed (home-version2.html)
-  window.onorientationchange = function() {
-    var orientation = window.orientation;
-    if( $('.carousel-container').length>0 ){
-      switch(orientation) {
-        case 0:
-        case 90:
-        case -90: window.location.reload();
-        break;
-      }
-    }
-  };
-
-  ////// parallax elements starts
-  if ($(window).width() > 768) {
-    $('.parallax-block, .pricing-banner').parallax("50%", -0.6);
-    $('.pricing-banner').parallax("50%", 1.3);
-    $('.block1').parallax("50%", -0.2);
-    $('.block2').parallax("50%", -0.2);
-    $('.block3').parallax("50%", -0.2);
-    $('.block4').parallax("50%", -0.2);
-    $('.block5').parallax("50%", -0.2);
-    $('.block6').parallax("50%", -0.2);
-    $('.block7').parallax("50%", -0.2);
-  }
-
-  ////// Wow Script starts
-  var wow = new WOW({
-    animateClass: 'animated',
-    offset: 100,
-    callback: function(box) {
-
-    }
-  });
-  wow.init();
+  "use strict";  
 
   //////scroll function for changed css starts
   $(window).scroll(function() {
@@ -53,93 +17,8 @@ $(function(){
   });
 
   //////show loader image on loading starts
-  $(".se-pre-con").fadeOut("slow");
-
-  ///// testimonial bx slider starts
-  $('.bxslider').bxSlider({
-    minSlides: 1,
-    maxSlides: 1,
-    slideMargin: 0,
-    mode: 'fade',
-    speed: 1000,
-    easing: 'swing',
-    auto: true
-  });
-
-  ///// sucess slider
-  $('.sucess-slide').bxSlider({
-    auto: true,
-    pager: false,
-    controls: true,
-    prevText : '<i class="fa fa-angle-left"></i> Prev ',
-    nextText : 'Next <i class="fa fa-angle-right"> </i>'
-  });
-
-  ///// home banner starts
-  $("#owl-demo").owlCarousel({
-  autoPlay: 4000, //Set AutoPlay to 3 seconds
-  transitionStyle: "fade",
-  items: 1,
-  itemsDesktop: [1199, 1],
-  itemsDesktopSmall: [979, 1],
-  itemsMobile: [768, 1]
-
-  });
-
-  ///// featured menu carousel slider starts
-  var lw = 600;
-  var lh = 388;
-  var sw = 495;
-  var sh = 320;
-  var sp = 10;
-  var so = 30;
-  var tp = 10;
-
-  var $width = $(window).width();
-  if($width < 1200 && $width > 992){
-    lw = 500;
-    lh = 288;
-    sw = 295,
-    sh = 220,
-    sp = 10,
-    so = 30,
-    tp = 10;
-  } else if($width < 993 && $width >= 769){
-    lw = 400;
-    lh = 258;
-    sw = 205,
-    sh = 180,
-    sp = 10,
-    so = 30,
-    tp = 10;
-  } else if($width <= 768 && $width > 400){
-    lw = 250;
-    lh = 180;
-    sw = 195,
-    sh = 120,
-    sp = 10,
-    so = 30,
-    tp = 10;
-  } else if($width <= 400 ){
-    lw = 200;
-    lh = 150;
-    sw = 195,
-    sh = 120,
-    sp = 10,
-    so = 15,
-    tp = 20;
-  }
-
-  var carousel = $("#carousel").featureCarousel({
-    largeFeatureWidth : lw,
-    largeFeatureHeight : lh,
-    smallFeatureWidth : sw,
-    smallFeatureHeight : sh,
-    sidePadding : sp,
-    smallFeatureOffset : so,
-    topPadding : tp
-  });
-
+  $(".se-pre-con").fadeOut("slow"); 
+     
   ///////menu toggle starts
   $("a,div,button, span").on('click',function(e){
 
@@ -213,34 +92,7 @@ $(function(){
           inputScope.val(parseInt(inputScope.val())-1);
     }
   });
-
-  ///// featured menu bx slider starts
-  if ($(window).width() > 768) {
-    $('.bxslider1').bxSlider({
-      minSlides: 4,
-      maxSlides: 4,
-      adaptiveHeight: true,
-      slideWidth: 270,
-      slideMargin: 0,
-      speed: 1000,
-      easing: 'swing',
-      auto: true
-    });
-  }
-  else {
-
-    $('.bxslider1').bxSlider({
-      minSlides: 1,
-      maxSlides: 1,
-      adaptiveHeight: true,
-      slideWidth: 270,
-      slideMargin: 0,
-      speed: 1000,
-      easing: 'swing',
-      auto: true
-    });
-  };
-
+ 
   //remove class if browser is IE starts
   var ms_ie = false;
   var ua = window.navigator.userAgent;
@@ -269,29 +121,7 @@ $(function(){
   var $input = $('.txtacrescimo');
 
   // Set 0 to start
-  $input.val(1);
-
- 
-
-  //range slider
-  $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 1000,
-      values: [ 0, 800 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val(  ui.values[ 0 ] + " - " + ui.values[ 1 ] );
-      }
-    });
-    $( "#amount" ).val( + $( "#slider-range" ).slider( "values", 0 ) +
-      " - " + $( "#slider-range" ).slider( "values", 1 ) );
-
-
-  //counter
-  $('.counter').counterUp({
-      delay: 10,
-      time: 1000
-  });
+  $input.val(1); 
 
 });
 
